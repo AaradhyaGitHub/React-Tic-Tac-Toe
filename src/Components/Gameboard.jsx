a58dc58edde7c7d1aa92c8af9bb11656b15d8c34
Aaradhya Poudyal <aaradhyapoudyal@Aaradhyas-MacBook-Air.local>
2024-07-13 03:03:00 -0700

    Updating array immutably

--- src/Components/Gameboard.jsx
+++ src/Components/Gameboard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -7,14 +8,41 @@ const initialGameBoard = [
 ];
 
 export default function Gameboard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    {/*
+      setGameBoard((prevGameBoard) => {
+        prevGameBoard[rowIndex][colIndex] = "X";
+        return prevGameBoard;
+      });
+      
+            If your state is an object or an array, you shoulld update it in a immutable way 
+            Which MEANS.... Make a copy of the object or the array so a new array or a new object 
+            Then you change the copy and not the original. This is because when are modifying object 
+            
+            prevGameBoard[rowIndex][colIndex] = 'X'
+            Above code immiedeately updates the old value in the memory even before the scheduled update is handles by React
+            This can lead to bugs or side effects if you have multiple scheduled state updates in your code
+        */
+    }
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        //updatedBoard is now a new array that consists value of old array as child components
+        // now wer have a brand new array with brand new nested arrays which still stores the value it was holding on to before
+        //this is an immutable way of updating the array
+        updatedBoard[rowIndex][colIndex] = "X";
+        return updatedBoard;
+      });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button> {playerSymbol} </button>
+                <button onClick = {() => handleSelectSquare(rowIndex,colIndex)}> {playerSymbol} </button>
               </li>
             ))}
           </ol>
